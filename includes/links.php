1baf8d1953ed0c48026932d8b10598aa16a761b0
jazz <jazz.s@aiit.vic.edu.au>
2025-12-10 16:37:05 +1100

    push code

--- includes/links.php
+++ includes/links.php
@@ -1,4 +1,4 @@
-<?php define('BASE_PATH',"/aiit College/aiit-main/");?>
+<?php define('BASE_PATH',"/aiit-main/");?>
 <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="img/favicon//favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="img/favicon//favicon-16x16.png">
