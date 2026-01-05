0080a4ae71d3faaf1e8d3461e0e03fd864a4c610
jatinder <jssingh134@gmail.com>
2026-01-05 18:00:24 +1100

    update about img

--- aboutus.php
+++ aboutus.php
@@ -3,130 +3,145 @@
 
 
 <head>
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About Us | AIIT Melbourne</title>
-  <meta name="description" content="Learn about AIIT Melbourne, offering top cookery & leadership training. Discover our mission.">
-  <meta name="keywords" content="AIIT about us, who is AIIT Melbourne, AIIT college history,Cookery courses Melbourne,best cookery courses in Melbourne,Melbourne Cookery College,Commercial cookery college near to the Flinders Street,Leadership courses Melbourne CBD,Commercial cookery training in Melbourne CBD,Diploma of Leadership and Management Melbourne,Vocational training Melbourne,International student courses Melbourne">
-  <meta name="robots" content="index, follow">
-  <link rel="canonical" href="https://www.aiit.edu.au/aboutus.php">
-  <meta property="og:title" content="Contact Us| AIIT Melbourne">
-  <meta property="og:description" content="Learn about AIIT Melbourne, offering top cookery & leadership training. Discover our mission.">
-  <meta property="og:image" content="https://www.aiit.edu.au/img/logo.png">
-  <meta property="og:url" content="https://www.aiit.edu.au/courses/aboutus.php">
-  <meta property="og:type" content="website">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us | AIIT Melbourne</title>
+    <meta name="description"
+        content="Learn about AIIT Melbourne, offering top cookery & leadership training. Discover our mission.">
+    <meta name="keywords"
+        content="AIIT about us, who is AIIT Melbourne, AIIT college history,Cookery courses Melbourne,best cookery courses in Melbourne,Melbourne Cookery College,Commercial cookery college near to the Flinders Street,Leadership courses Melbourne CBD,Commercial cookery training in Melbourne CBD,Diploma of Leadership and Management Melbourne,Vocational training Melbourne,International student courses Melbourne">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="https://www.aiit.edu.au/aboutus.php">
+    <meta property="og:title" content="Contact Us| AIIT Melbourne">
+    <meta property="og:description"
+        content="Learn about AIIT Melbourne, offering top cookery & leadership training. Discover our mission.">
+    <meta property="og:image" content="https://www.aiit.edu.au/img/logo.png">
+    <meta property="og:url" content="https://www.aiit.edu.au/courses/aboutus.php">
+    <meta property="og:type" content="website">
     <?php include_once('./includes/links.php') ?>
 </head>
 
 <body>
-    
-<?php include_once('./includes/header.php') ?>
-    
+
+    <?php include_once('./includes/header.php') ?>
+
     <section class="headersection">
         <div class="container">
             <h1 class="pageHeading">about us </h1>
             <p><a href="">Home</a> / <a href="">about us</a></p>
         </div>
     </section>
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="abouttxt">
-                    <h1 class="heading">about us</h1>
-                    <p>Australian International Institute of Technology Pty Ltd ATF AIIT Unit Trust trading as
-                        Australian International Institute of Technology is a CRICOS Registered Training Organisation
-                        based in Melbourne, Australia.
-                        <br>
-                        <br>
-                        Australian International Institute of Technology is accredited and endorsed by the Department of Education and the National Regulator, Australian Skills Quality Authority (ASQA), and provides nationally recognised training, qualifications, and accredited courses in the following disciplines:</p>
-                    
-                    <ul class="row">
-                        <li class="col-sm-6"><img src="img/icons/check1.png" width="20px"  alt="">
-                            <a href=""> General English <small> (Beginner to Advanced Level)</small> </a>
-                        </li>
-                        <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt=""><a href=""> Leadership and Management </a></li>
-                        <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt=""><a href=""> Commercial Cookery </a></li>
-                        <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt=""><a href=""> Hospitality Management</a></li>
+    <section>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="abouttxt">
+                        <h1 class="heading">about us</h1>
+                        <p>Australian International Institute of Technology Pty Ltd ATF AIIT Unit Trust trading as
+                            Australian International Institute of Technology is a CRICOS Registered Training
+                            Organisation
+                            based in Melbourne, Australia.
+                            <br>
+                            <br>
+                            Australian International Institute of Technology is accredited and endorsed by the
+                            Department of Education and the National Regulator, Australian Skills Quality Authority
+                            (ASQA), and provides nationally recognised training, qualifications, and accredited courses
+                            in the following disciplines:
+                        </p>
 
-                    </ul>
-                    
-                    <br>
-                    <p>
-                        We aim to provide our students a supportive and caring learning environment. Our qualified and accredited academic staff and our administrative support team aim is your success and we will do everything we can to support and help you to achieve your study goals.
-<br><br>
-Our mission and agenda is simple; to empower individuals for the world of tomorrow.
-                    </p>
-                    
+                        <ul class="row">
+                            <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt="">
+                                <a href=""> General English <small> (Beginner to Advanced Level)</small> </a>
+                            </li>
+                            <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt=""><a href="">
+                                    Leadership and Management </a></li>
+                            <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt=""><a href="">
+                                    Commercial Cookery </a></li>
+                            <li class="col-sm-6"><img src="img/icons/check1.png" width="20px" alt=""><a href="">
+                                    Hospitality Management</a></li>
+
+                        </ul>
+
+                        <br>
+                        <p>
+                            We aim to provide our students a supportive and caring learning environment. Our qualified
+                            and accredited academic staff and our administrative support team aim is your success and we
+                            will do everything we can to support and help you to achieve your study goals.
+                            <br><br>
+                            Our mission and agenda is simple; to empower individuals for the world of tomorrow.
+                        </p>
+
+                    </div>
+
                 </div>
-              
-            </div>
-            <div class="col-md-4">
-                <div class="whyimg">
-                    <img src="https://img.freepik.com/premium-photo/two-people-kitchen-with-food-counter_829667-8928.jpg?w=740" class="img-fluid" alt="">
+                <div class="col-md-4">
+
+                    <div class="whyimg">
+                        <img src="img/aboutus.webp" class="img-fluid" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section>
-    <div class="container">
-        <h1 class="heading">we provide</h1>
-        <div class="row">
-            <div class="col-md-3 col-6">
-                <div class="sbox greenbox ">
-                    <div class="sicon greenicon text-center">
-                        <img src="img/icons/teacher.png" width="64px" class="img-fluid" alt="">
+    <section>
+        <div class="container">
+            <h1 class="heading">we provide</h1>
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <div class="sbox greenbox ">
+                        <div class="sicon greenicon text-center">
+                            <img src="img/icons/teacher.png" width="64px" class="img-fluid" alt="">
+                        </div>
+                        <br>
+                        <div class="stxt">
+                            <h6>Experienced Faculty</h6>
+                            <p>Passionate educators with expertise offer personalised insights and guidance.</p>
+                        </div>
                     </div>
-                    <br>
-                    <div class="stxt">
-                        <h6>Experienced Faculty</h6>
-                        <p>Passionate educators with  expertise offer personalised insights and guidance.</p>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="sbox yellow">
+                        <div class="sicon yellowicon text-center">
+                            <img src="img/icons/leadership.png" width="64px" class="img-fluid" alt="">
+                        </div>
+                        <br>
+                        <div class="stxt">
+                            <h6>Career Support</h6>
+                            <p>Help with interview prep, resumes, and internships connect you to employers. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="sbox red">
+                        <div class="sicon redicon text-center">
+                            <img src="img/icons/academic.png" width="64px" class="img-fluid" alt="">
+                        </div>
+                        <br>
+                        <div class="stxt">
+                            <h6>Academic Excellence</h6>
+                            <p>ASQA-backed programs deliver industry-aligned curricula for nationwide recognition.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="sbox black">
+                        <div class="sicon blackicon text-center">
+                            <img src="img/icons/exchange.png" width="64px" class="img-fluid" alt="">
+                        </div>
+                        <br>
+                        <div class="stxt">
+                            <h6>Global Perspective</h6>
+                            <p>Diverse students from different countries foster intercultural exchange and partnerships.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 col-6">
-                <div class="sbox yellow">
-                    <div class="sicon yellowicon text-center">
-                        <img src="img/icons/leadership.png" width="64px" class="img-fluid" alt="">
-                    </div>
-                    <br>
-                    <div class="stxt">
-                        <h6>Career Support</h6>
-                        <p>Help with interview prep, resumes, and internships connect you to  employers.  </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-6">
-                <div class="sbox red">
-                    <div class="sicon redicon text-center">
-                        <img src="img/icons/academic.png" width="64px" class="img-fluid" alt="">
-                    </div>
-                    <br>
-                    <div class="stxt">
-                        <h6>Academic Excellence</h6>
-                        <p>ASQA-backed programs deliver industry-aligned curricula for nationwide recognition.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-6">
-                <div class="sbox black">
-                    <div class="sicon blackicon text-center">
-                        <img src="img/icons/exchange.png" width="64px" class="img-fluid" alt="">
-                    </div>
-                    <br>
-                    <div class="stxt">
-                        <h6>Global Perspective</h6>
-                        <p>Diverse students from different countries foster intercultural exchange and partnerships.</p>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    </div>
-      
-</section>
-<!-- 
+        </div>
+
+    </section>
+    <!-- 
 <section class="kitchenClass bggray">
     <div class="container">
         <div class="d-flex justify-content-between align-items-center" style="gap:20px;">
@@ -147,4 +162,4 @@ Our mission and agenda is simple; to empower individuals for the world of tomorr
        
     </div>
 </section> -->
-<?php include_once('./includes/footer.php') ?>
+    <?php include_once('./includes/footer.php') ?>
